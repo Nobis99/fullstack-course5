21a4b9147b5f8504191052572ecadb16b9c1c67c
Kane <kanepy@gmail.com>
2017-01-15 13:24:36 -0500

    completes min requirements for assignment1

--- assignments/assignment1/assignment1-starter-code/app.js
+++ assignments/assignment1/assignment1-starter-code/app.js
@@ -1,27 +1,38 @@
 (function () {
-'use strict';
-
-angular.module('LunchChecker', [])
+    'use strict';
+    angular.module('LunchChecker', [])
 
 .controller('MsgController', MsgController);
 MsgController.$inject = ['$scope'];
 
     
 function MsgController($scope) {
+    
     $scope.updating = function() {
         var boxMsg = $scope.dishes_list;
         $scope.TextBoxMsg = boxMsg;
     }
 
-    
-    $scope.CheckBox = function () {
-        $scope.TextBoxMsg = "There is value in the Text.";
-        //$scope.dishes_list = "";
+    $scope.Check_Dish = function () {
+        var boxMsg = $scope.dishes_list;
+        if (!boxMsg) {
+            $scope.TextBoxMsg = "Please enter data first";
+        }
+        if (boxMsg) {
+            var pList = boxMsg.split(',');
+            if (pList.length <= 3) {
+                $scope.TextBoxMsg = "Enjoy!";
+            }
+            else {
+                $scope.TextBoxMsg = "Too much!";
+            }
+        }
+        //$scope.TextBoxMsg = boxMsg;
     };
 }
 
-function CheckComma () {
-    
+function CheckComma (string) {
+    return 0;
 }
     
 function firstController ($scope) {
