973aebbab747a8f98d1bc6a8d32644a58d08d07a
Nobis99 <kanepy@gmail.com>
2017-02-08 03:34:33 -0500

    added ng-repeat test

--- assignments/assignment2/assignment2-starter-code/scripts/app.js
+++ assignments/assignment2/assignment2-starter-code/scripts/app.js
@@ -1,11 +1,33 @@
 (function () {
 'use strict';
+
+var shoppinglist1 = [
+    "Milk","Chocolate","Peanut Butter","Donuts"
+];
+
+var shoppinglist2 = [
+{
+    name : "Milk",
+    quantity: "2"
+},
+{
+    name : "Donuts",
+    quantity : "100"
+},
+{
+    name : "Apples",
+    quantity : "12"
+}
+];
+    
 angular.module('Shopping',[])
     .controller('CartController', CartController);
     CartController.$inject = ['$scope'];
 
     function CartController($scope) {
-        $scope.name = "Kane";
+        $scope.shoppinglist1 = shoppinglist1;
+        $scope.shoppinglist2 = shoppinglist2;
+        
     }
     
 })();
